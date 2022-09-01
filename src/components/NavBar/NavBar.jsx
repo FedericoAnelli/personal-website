3f3e09ac7d3b9711ec7e82acaf7ad57e0f1bec5f
Federico Anelli <federicoanelli@gmail.com>
2022-09-01 17:21:01 -0300

    Sidebar fixes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,11 @@ import "./NavBar.css";
 import githubIcon from "../../assets/icons/github.png";
 import linkedinIcon from "../../assets/icons/linkedin.png";
 import { useNavigate } from "react-router-dom";
+import infoIcon from "../../assets/icons/info.png";
+import experienceIcon from "../../assets/icons/experience.png";
+import educationIcon from "../../assets/icons/education.png";
+import certificationsIcon from "../../assets/icons/certifications.png";
+import projectsIcon from "../../assets/icons/projects.png";
 
 const NavBar = () => {
     const navigate = useNavigate();
@@ -15,11 +20,11 @@ const NavBar = () => {
             <ul>
                 <li className="profilePicLi"><div className="profilePic"></div></li>
                 <li className="myName">Federico Anelli</li>
-                <li><button onClick={handleClick}>About</button></li>
-                <li><button onClick={handleClick}>Experience</button></li>
-                <li><button onClick={handleClick}>Education</button></li>
-                <li><button onClick={handleClick}>Licenses & Certifications</button></li>
-                <li><button onClick={handleClick}>Projects</button></li>
+                <li><button onClick={handleClick}><div className="buttonDiv"><img className="buttonIcon" src={infoIcon} alt="about" />About</div></button></li>
+                <li><button onClick={handleClick}><div className="buttonDiv"><img className="buttonIcon" src={experienceIcon} alt="experience" />Experience</div></button></li>
+                <li><button onClick={handleClick}><div className="buttonDiv"><img className="buttonIcon" src={educationIcon} alt="education" />Education</div></button></li>
+                <li><button onClick={handleClick}><div className="buttonDiv"><img className="buttonIcon" src={certificationsIcon} alt="licenses and certifications" />Certifications</div></button></li>
+                <li><button onClick={handleClick}><div className="buttonDiv"><img className="buttonIcon" src={projectsIcon} alt="projects" />Projects</div></button></li>
             </ul>
             <div className="socialHandlesContainer">
                 <img className="socialHandles" src={linkedinIcon} alt="linkedin button" />
