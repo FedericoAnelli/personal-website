7b34a48ce07224fdb50f28e0a71bbcb07d173ed6
Federico Anelli <federicoanelli@gmail.com>
2022-09-01 18:45:43 -0300

    Perfected routing

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,12 @@
+import "./Experience.css";
+
+const Experience = () => {
+
+    return(
+        <div className="experienceBody">
+            <h1>Experience</h1>
+        </div>
+    )
+}
+
+export default Experience;
--- src/components/SectionContainer/SectionContainer.jsx
+++ src/components/SectionContainer/SectionContainer.jsx
@@ -1,13 +1,32 @@
 import "./SectionContainer.css";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Experience from "../Experience/Experience";
 
 const SectionContainer = () => {
 const { section } = useParams();
+const navigate = useNavigate();
+
+const validateSection = (section) => {
+   if (section === "About" || section === "Experience" || section === "Education" || section === "Certifications" || section === "Projects") {
+       return section;
+   }
+   else{
+      navigate("*");
+   }
+}
+
 
 return (
    <div className="sectionsBody">
         <div className="headerDiv">
-        {!section ? <h1>Home</h1> : <h1>{section}</h1>}
+        {!section ? <h1>Home</h1> : <h1>{validateSection(section)}</h1>}
+        </div>
+        <div>
+            {section === "About" ? <h1>About</h1> : null}
+            {section === "Experience" ? <Experience /> : null}
+            {section === "Education" ? <h1>Education</h1> : null}
+            {section === "Certifications" ? <h1>Certifications</h1> : null}
+            {section === "Projects" ? <h1>Projects</h1> : null}
         </div>
    </div> 
 )
