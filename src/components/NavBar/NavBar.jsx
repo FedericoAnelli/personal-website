e9e2560fdd2a2d8558fc226e4400b7ba0913ae36
Federico Anelli <federicoanelli@gmail.com>
2022-09-01 15:15:51 -0300

    Sidebar improvements

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,22 @@
 import "./NavBar.css";
 import React from "react";
+import githubIcon from "../../assets/icons/github.png";
+import linkedinIcon from "../../assets/icons/linkedin.png";
 
 const NavBar = () => {
     return (
         <div className="navbar">
             <ul>
-                <li>BUTTON 1</li>
-                <li>BUTTON 2</li>
-                <li>BUTTON 3</li>
+                <li className="profilePicLi"><div className="profilePic"></div></li>
+                <li className="myName">Federico Anelli</li>
+                <li><button>BUTTON 1</button></li>
+                <li><button>BUTTON 2</button></li>
+                <li><button>BUTTON 3</button></li>
             </ul>
+            <div className="socialHandlesContainer">
+                <img className="socialHandles" src={linkedinIcon} alt="linkedin button" />
+                <img className="socialHandles" src={githubIcon} alt="github button" />
+            </div>
         </div>
     );
     }
