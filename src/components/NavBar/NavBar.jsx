5940faed4f8e7da59fb7085037d7b2eec1ee6a1c
Federico Anelli <federicoanelli@gmail.com>
2022-09-01 11:34:58 -0300

    Sidebar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <div className="navbar">
+            <ul>
+                <li>BUTTON 1</li>
+                <li>BUTTON 2</li>
+                <li>BUTTON 3</li>
+            </ul>
+        </div>
+    );
+    }
+
+    export default NavBar;
