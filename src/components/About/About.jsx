ec658aaf6c832b35db3d7cf3ab8fe31f83772c38
Federico Anelli <federicoanelli@gmail.com>
2022-09-06 18:46:41 -0300

    Minor fixes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,15 +4,21 @@ const About = () => {
 
     return (
         <div className="about">
-        <p>My name is <strong>Federico Anelli</strong> and I am a <strong>Computer Systems Engineer</strong> currently living in <strong>Buenos Aires, Argentina.</strong></p>
+        <p>My name is <strong>Federico Anelli</strong> and I am a <strong>Computer Systems Engineer</strong> living in <strong>Buenos Aires, Argentina 🇦🇷. </strong></p>
         
-        <p><br />While I currently have the <strong>ReactJS Front End Developer</strong> certification, I'm currently studying <strong>Backend Development @ Coderhouse</strong> to obtain my <strong>Full Stack Developer</strong> certification.</p>
+        <p><br />I'm currently studying <strong>Backend Development @ Coderhouse</strong> to get my <strong>Full Stack Developer</strong> certification, after having recently obtained my <strong>Front End Developer</strong> certification.</p>
         
         <p><br />I'm also a <strong>Professional Scrum Product Owner (PSPO I)</strong> certified by <strong>Scrum.org</strong>.</p>
 
         <p><br />Currently, I am <strong>Head of Digital Product & Technology</strong> for <strong>Radio Disney in Latin America</strong> (Argentina, Brazil, Mexico, Chile, Uruguay, Ecuador, Bolivia, Paraguay, Peru, Panama, Dominican Republic & Costa Rica).</p>
         
-        <p><br />While I'm open to relocation opportunities to the EU or USA, I'm also available for Front End Development projects or Digital consulting</p>
+        <p><br />While I'm open to relocation opportunities to the EU or USA, I'm also available for Front End Development projects or Digital consulting.</p>
+
+        <p><br />My passion for building digital products started back when I was 11 years old in 2001. Back then, I used to spend most of my free time creating websites at GeoCities (amazing times).</p>
+
+        <p><br />This is the first website I've ever made:</p>
+
+        <p><a href="https://www.geocities.ws/dumbledore_321/index-2.html" target="_blank">https://www.geocities.ws/dumbledore_321/index-2.html</a></p>
         </div>
     );
 
