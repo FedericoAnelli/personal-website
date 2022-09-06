5c2ed57c492a0917deab897213d99aa1d5e334fc
Federico Anelli <federicoanelli@gmail.com>
2022-09-06 14:22:51 -0300

    Minor cosmetic changes

---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,12 @@
+import "./Education.css";
+
+const Education = () => {
+
+    return (
+        <div className="education">
+        <h1>Education</h1>
+        </div>
+    );
+    }
+
+export default Education;
--- src/components/SectionContainer/SectionContainer.jsx
+++ src/components/SectionContainer/SectionContainer.jsx
@@ -4,6 +4,9 @@ import Experience from "../Experience/Experience";
 import Certifications from "../Certifications/Certifications";
 import Submenu from "../Submenu/Submenu";
 import { useState } from "react";
+import Projects from "../Projects/Projects";
+import Education from "../Education/Education";
+import About from "../About/About";
 
 const SectionContainer = () => {
 const { section } = useParams();
@@ -11,8 +14,11 @@ const navigate = useNavigate();
 const [certificates, setCertificates] = useState("Development");
 
 const validateSection = (section) => {
-   if (section === "About" || section === "Experience" || section === "Education" || section === "Certifications" || section === "Projects") { 
+   if (section === "Experience" || section === "Education" || section === "Certifications" || section === "Projects") { 
       return section;
+   } else if (section === "About")
+   {
+      return "Hello!";
    }
    else{
       navigate("*");
@@ -31,14 +37,13 @@ return (
                {!section ? <h1>Home</h1> : <h1>{validateSection(section)}</h1>}
          </div>
         {section === "Certifications" ? <div className="headerSubMenu"><Submenu handleClick={handleClick} certificates={certificates} section="Certifications" /></div> : null}
-        {section === "Projects" ? <div className="headerSubMenu"><Submenu handleClick={handleClick} certificates={certificates} section="Projects" /></div> : null}
         </div>
         <div className="contentNavigator">
-            {section === "About" ? <h1>About</h1> : null}
+            {section === "Hello!" ? <About /> : null}
             {section === "Experience" ? <Experience /> : null}
-            {section === "Education" ? <h1>Education</h1> : null}
+            {section === "Education" ? <Education /> : null}
             {section === "Certifications" ? <Certifications certificates={certificates}/> : null}
-            {section === "Projects" ? <h1>Projects</h1> : null}
+            {section === "Projects" ? <Projects /> : null}
         </div>
    </div> 
 )
