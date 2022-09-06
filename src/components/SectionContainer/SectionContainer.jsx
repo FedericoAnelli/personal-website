0248e77137d30f5cce30f45e68cbfd65653ec132
Federico Anelli <federicoanelli@gmail.com>
2022-09-06 16:59:54 -0300

    Changed home

--- src/components/SectionContainer/SectionContainer.jsx
+++ src/components/SectionContainer/SectionContainer.jsx
@@ -34,12 +34,13 @@ return (
    <div className="sectionsBody">
         <div className="headerDiv">
             <div className="headerTitle">
-               {!section ? <h1>Home</h1> : <h1>{validateSection(section)}</h1>}
+               {!section ? <h1>{validateSection("About")}</h1> : <h1>{validateSection(section)}</h1>}
          </div>
         {section === "Certifications" ? <div className="headerSubMenu"><Submenu handleClick={handleClick} certificates={certificates} section="Certifications" /></div> : null}
         {section === "Projects" ? <div className="headerSubMenu"><Submenu section="Projects" /></div> : null}
         </div>
         <div className="contentNavigator">
+            {!section ? <About /> : null}
             {section === "About" ? <About /> : null}
             {section === "Experience" ? <Experience /> : null}
             {section === "Education" ? <Education /> : null}
