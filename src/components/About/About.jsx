e6285ca535e3ede2252e2032a215e0b0d282b928
Federico Anelli <federicoanelli@gmail.com>
2022-09-14 18:49:37 -0300

    Added more lines in about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,20 +5,24 @@ const About = () => {
     return (
         <div className="about">
         <p>My name is <strong>Federico Anelli</strong> and I am a <strong>Computer Systems Engineer</strong> living in <strong>Buenos Aires, Argentina 🇦🇷. </strong></p>
-        
+        <br />
         <p><br />I'm currently studying <strong>Backend Development @ Coderhouse</strong> to get my <strong>Full Stack Developer</strong> certification, after having recently obtained my <strong>Front End Developer</strong> certification.</p>
-        
-        <p><br />I'm also a <strong>Professional Scrum Product Owner (PSPO I)</strong> certified by <strong>Scrum.org</strong>.</p>
-
-        <p><br />Currently, I am <strong>Head of Digital Product & Technology</strong> for <strong>Radio Disney in Latin America</strong> (Argentina, Brazil, Mexico, Chile, Uruguay, Ecuador, Bolivia, Paraguay, Peru, Panama, Dominican Republic & Costa Rica).</p>
-        
-        <p><br />While I'm open to relocation opportunities to the EU or USA, I'm also available for Front End Development projects or Digital consulting.</p>
-
-        <p><br />My passion for building digital products started back when I was 11 years old in 2001. Back then, I used to spend most of my free time creating websites at GeoCities (amazing times).</p>
-
-        <p><br />This is the first website I've ever made:</p>
-
-        <p><a href="https://www.geocities.ws/dumbledore_321/index-2.html" target="_blank">https://www.geocities.ws/dumbledore_321/index-2.html</a></p>
+        <br />
+        <p>I'm also a <strong>Professional Scrum Product Owner (PSPO I)</strong> certified by <strong>Scrum.org</strong>.</p>
+        <br />
+        <p>Currently, I am <strong>Head of Digital Product & Technology</strong> for <strong>Radio Disney in Latin America</strong> (Argentina, Brazil, Mexico, Chile, Uruguay, Ecuador, Bolivia, Paraguay, Peru, Panama, Dominican Republic & Costa Rica).</p>
+        <br />
+        <p>While I'm open to relocation opportunities to the EU or USA, I'm also available for Front End Development projects or Digital consulting.</p>
+        <br />
+        <p>My passion for building digital products started back when I was 11 years old in 2001. Back then, I used to spend most of my free time creating websites at GeoCities (amazing times).</p>
+        <br />
+        <p>This is the first website I've ever made:</p>
+        <br />
+        <p><a href="https://www.geocities.ws/dumbledore_321/index-2.html" target="_blank">Hogwarts Online School of Witchcraft & Wizardry</a></p>
+        <br />
+        <p>Besides becoming a web development enthusiast, I also enjoyed gaming a lot. One of my favorite games when I was younger was Disney's Virtual Magic Kingdom (an online MMORPG). I enjoyed the game so much that I was not only active in the game, but also outside it, joining different Forums and IRC Chat Channels to further enhance my experience. This led me to join VMK Magic, an online forum for the game which I ended up co-administrating. VMK Magic was the second largest Forum for the game. </p>
+        <br />
+        <p><a href="https://www.geocities.ws/dumbledore_321/index-2.html" target="_blank">VMK Magic</a></p>
         </div>
     );
 
