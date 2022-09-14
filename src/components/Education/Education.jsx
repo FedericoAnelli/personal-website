5345de860d508063f5732c3e7800dc8f27bd48f8
Federico Anelli <federicoanelli@gmail.com>
2022-09-14 18:41:27 -0300

    Added education

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,10 +1,20 @@
 import "./Education.css";
+import usalLogo from "../../assets/images/usal.png";
 
 const Education = () => {
 
     return (
-        <div className="education">
-        <h1>Education</h1>
+        <div className="educationBody">
+            <div className="education">
+
+                <img className="usalLogo" src={usalLogo} alt="logo universidad del salvador" />
+                <div className="textSection">
+                    <h2>Computer Systems Engineering</h2>
+                    <h3>Universidad del Salvador</h3>
+                    <p>5 Year Bachelor's Degree</p>
+                    <p>February 2021</p>
+                </div>
+            </div>
         </div>
     );
     }
